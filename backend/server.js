07967dfc423cce0132743f2aca85bd23bce7a5b1
lashaks <florentlogicafx@gmail.com>
2026-04-15 13:34:28 +0200

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 // ============================================================
 // MIDDLEWARE
 // ============================================================
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
